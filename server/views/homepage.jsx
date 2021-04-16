67ab38e1bfeb7bd022b73f51d435a7ab50cdfb91
Rhianna S <rhiannastuart@hotmail.co.uk>
2021-04-16 18:01:24 +0100

    grab api data

--- server/views/homepage.jsx
+++ server/views/homepage.jsx
@@ -10,14 +10,7 @@ const homepage = props => {
         </head>
         <body>
             <h1>Disaster hub</h1>
-           <p> {props.data[0]} </p>
-           <p> {props.data[36]} </p>
-           <p> {props.data[35]} </p>
-           <p> {props.data[36]} </p>
-           <p> {props.data[4]} </p>
-           <p> {props.data[5]} </p>
-           <p> {props.data[6]} </p>
-           <p> {props.data[7]} </p>
+          
            <p> {props.data[8]} </p>
            
            <p> {props.data[40]} </p>
