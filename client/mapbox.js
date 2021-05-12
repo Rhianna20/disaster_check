f7beae3614d755ff8b0662b22ff5c81161160072
Rhianna S <rhiannastuart@hotmail.co.uk>
2021-05-12 13:26:04 +0100

    pop up message created!!

--- client/mapbox.js
+++ client/mapbox.js
@@ -70,15 +70,10 @@ map.on("load", function () {
 map.on("click", "drone", function (e) {
 
   const coordinates = e.features[0].geometry.coordinates.slice();
-  const description = e.features[0].properties.description;
+  const description = e.features[0].properties.title;
 
    
-  // Ensure that if the map is zoomed out such that multiple
-  // copies of the feature are visible, the popup appears
-  // over the copy being pointed to.
-  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-  coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-  }
+
    
   new mapboxgl.Popup()
   .setLngLat(coordinates)
@@ -86,13 +81,5 @@ map.on("click", "drone", function (e) {
   .addTo(map);
   });
    
-  // Change the cursor to a pointer when the mouse is over the places layer.
-  map.on('mouseenter', 'places', function () {
-  map.getCanvas().style.cursor = 'pointer';
-  });
-   
-  // Change it back to a pointer when it leaves.
-  map.on('mouseleave', 'places', function () {
-  map.getCanvas().style.cursor = '';
-    });
+
   
